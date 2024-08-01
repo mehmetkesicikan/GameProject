97fe1fa14f7f0b7c4e454ab6a2297bcec0af2be5
mertkesicikan <mertkesicikan@MERTKESICIK07E7>
2024-08-01 15:14:30 +0300

    Add project files.

---
+++ GameProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer 
+            {
+                Id = 1, BirthYear = 1985, 
+                FirstName = "Mert", 
+                LastName = "K.", 
+                IdentityNumber = 1234 
+            });
+        }
+    }
+}
